bf9813d7c29d485316d0b3e9caf7ab1a14bc5556
lauramiotto5 <lauramiotto123@gmail.com>
2026-07-11 21:25:29 -0300

    Update Vercel response body

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 def app(environ, start_response):
     status = "200 OK"
     headers = [("Content-Type", "text/plain; charset=utf-8")]
-    body = b"Micro-Milling App is running"
+    body = "Micro-Milling App is running"
     start_response(status, headers)
-    return [body]
+    return [body.encode("utf-8")]
 
 
 application = app
